0909c6193403863d5ccc149c86b213127c241c93
Muhammad Milon <mmilon82814@gmail.com>
2025-12-04 02:10:10 +0600

    Add react-hot-toast dependency and implement routing for Login and Signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-grow">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
       </main>
       <Footer />
     </div>
